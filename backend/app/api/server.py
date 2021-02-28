807131b84f1d9989486b076bae9a67ea674024e7
Kevin Chan <kevinmchan@live.com>
2021-02-28 12:35:22 -0500

    Set up DB connections

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from ..core import config, predictions
+
 from .routes import router as api_router
 
 
-app = FastAPI(title="analyticsfans", version="0.1.0")
+def get_application():
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-app.include_router(api_router, prefix="/api")
+    app.add_event_handler("startup", predictions.create_start_app_handler(app))
+    app.add_event_handler("shutdown", predictions.create_stop_app_handler(app))
+
+    app.include_router(api_router, prefix="/api")
+
+    return app
+
+
+app = get_application()
